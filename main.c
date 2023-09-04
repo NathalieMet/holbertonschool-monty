6bacae402cdc950ff92f8cab09809801411ad1be
nathalie <nathalie.metreau@hotmail.fr>
2023-09-04 14:15:31 +0200

    main and create node

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+/**
+ * _printf - Write function that produces output according to
+ * format.
+ * @format: The character to print
+ *
+ * Return: On success 1.
+ *
+ */
+int main ()
+{
+return(0);
+}
