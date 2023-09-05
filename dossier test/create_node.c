fd927e152d7f7f2f10497803dd4c080b3239a65f
nathalie <nathalie.metreau@hotmail.fr>
2023-09-05 12:32:09 +0200

    push_and_pall

---
+++ dossier test/create_node.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+/**
+ * create_node - 
+ * @format: The character to print
+ *
+ * Return: On success 1.
+ *
+ */
+stack_t *create_node (int file_value, stack_t *head)
+{
+stack_t *new_node = malloc(sizeof(stack_t));
+if (new_node == NULL)
+exit (EXIT_FAILURE);
+new_node->n = file_value;
+new_node->next = head;
+new_node->prev = NULL;
+head->prev = new_node;
+return(new_node);
+}
---
+++ dossier test/main.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DELIMS " \n\t\r"
+/**
+ * main - Monty interpreter
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ *
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	char *line = NULL, *opcode;
+	size_t len = 0;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
+	instruction_t opcodes[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+		};
+	int i;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &len, file) != -1)
+	{
+		line_number++;
+		opcode = strtok(line, DELIMS);
+		if (opcode == NULL || opcode[0] == '#')
+			continue;
+		for (i = 0; opcodes[i].opcode; i++)
+		{
+			if (strcmp(opcode, opcodes[i].opcode) == 0)
+			{
+				opcodes[i].f(&stack, line_number);
+				break;
+			}
+		}
+		if (opcodes[i].opcode == NULL)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+			exit(EXIT_FAILURE);
+		}
+	}
+	free(line);
+	fclose(file);
+	return (EXIT_SUCCESS);
+}
